66f5e622561474821bde1f957e4a82e962cfb867
Denis Angell <dangell@transia.co>
2023-02-04 01:06:37 -0500

    fix features bug

--- py/core/rippled/utils.py
+++ py/core/rippled/utils.py
@@ -7,11 +7,28 @@ from typing import Dict, Any # noqa: F401
 from core.common.utils import read_file
 
 
-def parse_rippled_features(path: str):
-    result = read_file(path)
-    # find all the extern uint256 const variables
-    pattern = r'extern uint256 const (\w+);'
-    matches = re.findall(pattern, result)
+def parse(value: str):
+    if value == 'no':
+        return False
+    else:
+        return True
 
-    # return the list of variables
-    return matches
+def parse_rippled_features(path: str):
+    with open(path, 'r') as f:
+        lines = f.readlines()
+        features = {}
+        for line in lines:
+            if re.match(r'REGISTER_FEATURE', line) or re.match(r'REGISTER_FIX', line):
+                feature_name: str = ''
+                if re.match(r'REGISTER_FIX', line):
+                    feature_name = re.search('REGISTER_FIX\)?.*?\((.*?),', line).group(1) or 0
+                if re.match(r'REGISTER_FEATURE', line):
+                    feature_name = re.search('REGISTER_FEATURE\((.*?),', line).group(1) or 0
+                supported = re.findall(r'Supported::(.*),', line)
+                default_vote = re.findall(r'DefaultVote::(.*),', line)
+                features[feature_name] = {
+                    'supported': parse(supported[0] if supported else 'no'),
+                    'default_vote': parse(default_vote[0] if default_vote else 'no')
+                }
+    print([k for k, v in features.items() if v['supported'] == True])
+    return [k for k, v in features.items() if v['supported'] == True]
--- py/tests/unit/rippled/test_rippled_utils.py
+++ py/tests/unit/rippled/test_rippled_utils.py
@@ -14,9 +14,8 @@ logger = logging.getLogger('app')
 
 class TestRippledUtils(BaseTestConfig):
 
-    feature_list: List[str] = ['featureOwnerPaysFee', 'featureFlow', 'featureFlowCross', 'featureCryptoConditionsSuite', 'fix1513', 'featureDepositAuth', 'featureChecks', 'fix1571', 'fix1543', 'fix1623', 'featureDepositPreauth', 'fix1515', 'fix1578', 'featureMultiSignReserve', 'fixTakerDryOfferRemoval', 'fixMasterKeyAsRegularKey', 'fixCheckThreading', 'fixPayChanRecipientOwnerDir', 'featureDeletableAccounts', 'fixQualityUpperBound', 'featureRequireFullyCanonicalSig', 'fix1781', 'featureHardenedValidations', 'fixAmendmentMajorityCalc', 'featureNegativeUNL', 'featureTicketBatch', 'featureFlowSortStrands', 'fixSTAmountCanonicalize', 'fixRmSmallIncreasedQOffers', 'featureCheckCashMakesTrustLine', 'featureNonFungibleTokensV1', 'featureExpandedSignerList', 'fixNFTokenDirV1', 'fixNFTokenNegOffer', 'featureNonFungibleTokensV1_1', 'fixTrustLinesToSelf', 'fixRemoveNFTokenAutoTrustLine', 'featureImmediateOfferKilled']
+    feature_list: List[str] = ['Flow', 'FlowCross', 'CryptoConditionsSuite', 'fix1513', 'DepositAuth']
 
     def test_parse_rippled_features(cls):
         result: Any = parse_rippled_features('./tests/fixtures/core.h')
         cls.assertEqual(result, cls.feature_list)
-
