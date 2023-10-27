24e9c2e1e4dedb849c26efd80b3e2aa29c9fd6c6
Denis Angell <dangell@transia.co>
2023-10-27 10:21:23 +0200

    update and publish

--- py/README.md
+++ py/README.md
@@ -5,3 +5,9 @@ export GOOGLE_APPLICATION_CREDENTIALS="$(pwd)/credentials.json"
 `poetry run python3 playground.py`
 
 `poetry run python3 test.py tests/unit`
+
+# Publish
+
+`poetry build`
+
+`poetry publish`
--- py/playground.py
+++ py/playground.py
@@ -1,9 +1,2 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-from xrpl_helpers.rippled.utils import parse_version_from_path
-
-rippled_dir: str = "/Users/denisangell/projects/xrpl-labs/xahaud"
-features_file: str = rippled_dir + "/src/ripple/protocol/impl/BuildInfo.cpp"
-version_str: str = parse_version_from_path(features_file)
-print(version_str)
